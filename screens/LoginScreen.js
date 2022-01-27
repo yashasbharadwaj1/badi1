c0a03acfa61a3d798c38d4366c61680a236ec13b
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-01-27 17:11:32 +0530

    second commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text ,KeyboardAvoidingView, TextInput} from 'react-native';
 import React from 'react';
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+      <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+      >
+      <View style={styles.inputContainer}>
+       <TextInput
+         placeholder="Email"
+         style={styles.input}
+         
+         
+         />
+
+       <TextInput
+       
+       placeholder="Password"
+       style={styles.input}
+       secureTextEntry
+       
+       
+       />
+      
+      </View>
+    
+    </KeyboardAvoidingView>
   );
 }
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
